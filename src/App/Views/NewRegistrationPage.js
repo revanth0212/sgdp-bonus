f72c7749bda4aa18e8f6d93bb07d8abbd7ccf477
Revanth <revanth0212@gmail.com>
2018-10-02 23:52:20 -0500

    Styling changes.

--- src/App/Views/NewRegistrationPage.js
+++ src/App/Views/NewRegistrationPage.js
@@ -6,53 +6,61 @@ import PhoneNumberField from '../Shared/PhoneNumberField'
 import badgeSVG from './id-badge-solid.svg'
 
 export default () => (
-  <div>
-    <h3 style={{ paddingBottom: '2%', textAlign: 'center' }}>New Registration Page</h3>
-    <div style={{ display: 'flex' }}>
-      <div style={{ flex: '0.3', float: 'left' }}>
-        <img src={badgeSVG} alt="Registration Icon" style={{ padding: '2% 13%', height: '250px', width: '250px' }} />
-      </div>
-      <div style={{ flex: '0.7', float: 'right', height: 'fit-content' }}>
-        <div className="row">
-          <div className="col-5">
-            <TextField name="firstName" path={['fields', 'firstName']} />
-          </div>
-          <div className="col-2">
-            <TextField name="middleName" path={['fields', 'middleName']} />
-          </div>
-          <div className="col-5">
-            <TextField name="lastName" path={['fields', 'lastName']} />
-          </div>
+  <div style={{ width: '100vw', height: '100vh', background: 'rgba(225,115,0,0.5)' }}>
+    <div
+      style={{
+        margin: 'auto',
+        width: '65%',
+        paddingTop: '2%'
+      }}
+    >
+      <h3 style={{ paddingBottom: '2%', textAlign: 'center' }}>User Registration Page (Updated UI)</h3>
+      <div style={{ display: 'flex' }}>
+        <div style={{ flex: '0.3', float: 'left' }}>
+          <img src={badgeSVG} alt="Registration Icon" style={{ padding: '2% 13%', height: '250px', width: '250px' }} />
         </div>
-        <div className="row">
-          <div className="col-6">
-            <TextField name="address" path={['fields', 'address']} />
+        <div style={{ flex: '0.7', float: 'right', height: 'fit-content' }}>
+          <div className="row">
+            <div className="col-5">
+              <TextField name="firstName" path={['fields', 'firstName']} />
+            </div>
+            <div className="col-2">
+              <TextField name="middleName" path={['fields', 'middleName']} />
+            </div>
+            <div className="col-5">
+              <TextField name="lastName" path={['fields', 'lastName']} />
+            </div>
           </div>
-          <div className="col-3">
-            <TextField name="zipCode" path={['fields', 'zipCode']} />
+          <div className="row">
+            <div className="col-6">
+              <TextField name="address" path={['fields', 'address']} />
+            </div>
+            <div className="col-3">
+              <TextField name="zipCode" path={['fields', 'zipCode']} />
+            </div>
+            <div className="col-3">
+              <TextField name="city" path={['fields', 'city']} />
+            </div>
           </div>
-          <div className="col-3">
-            <TextField name="city" path={['fields', 'city']} />
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-3">
-            <DropDownField name="state" path={['fields', 'state']} />
-          </div>
-          <div className="col-3">
-            <DropDownField name="country" path={['fields', 'country']} />
-          </div>
-          <div className="col-6" style={{ marginTop: '0px' }}>
-            <PhoneNumberField
-              phoneNumberProps={{
-                name: 'phoneNumber',
-                path: ['fields', 'phoneNumber']
-              }}
-              phoneNumberCodeProps={{
-                name: 'phoneNumberCode',
-                path: ['fields', 'phoneNumberCode']
-              }}
-            />
+          <div className="row">
+            <div className="col-3">
+              <DropDownField name="state" path={['fields', 'state']} />
+            </div>
+            <div className="col-3">
+              <DropDownField name="country" path={['fields', 'country']} />
+            </div>
+            <div className="col-6" style={{ marginTop: '0px' }}>
+              <PhoneNumberField
+                phoneNumberProps={{
+                  name: 'phoneNumber',
+                  path: ['fields', 'phoneNumber']
+                }}
+                phoneNumberCodeProps={{
+                  name: 'phoneNumberCode',
+                  path: ['fields', 'phoneNumberCode']
+                }}
+              />
+            </div>
           </div>
         </div>
       </div>
--- src/App/Views/RegistrationPage.js
+++ src/App/Views/RegistrationPage.js
@@ -5,53 +5,61 @@ import DropDownField from '../Shared/DropDownField'
 import PhoneNumberField from '../Shared/PhoneNumberField'
 
 export default () => (
-  <div>
-    <h3 style={{ paddingBottom: '2%', textAlign: 'center' }}>Registration Page</h3>
-    <div className="row">
+  <div style={{ width: '100vw', height: '100vh' }}>
+    <div
+      style={{
+        margin: 'auto',
+        width: '65%',
+        paddingTop: '2%'
+      }}
+    >
+      <h3 style={{ paddingBottom: '2%', textAlign: 'center' }}>User Registration Page</h3>
       <div className="row">
-        <div className="col-4">
-          <TextField name="firstName" path={['fields', 'firstName']} />
-        </div>
-        <div className="col-4">
-          <TextField name="middleName" path={['fields', 'middleName']} />
-        </div>
-        <div className="col-4">
-          <TextField name="lastName" path={['fields', 'lastName']} />
-        </div>
-      </div>
-      <div className="row">
-        <div>
-          <div className="col-10">
-            <TextField name="address" path={['fields', 'address']} />
+        <div className="row">
+          <div className="col-4">
+            <TextField name="firstName" path={['fields', 'firstName']} />
           </div>
-          <div className="col-2">
-            <TextField name="zipCode" path={['fields', 'zipCode']} />
+          <div className="col-4">
+            <TextField name="middleName" path={['fields', 'middleName']} />
+          </div>
+          <div className="col-4">
+            <TextField name="lastName" path={['fields', 'lastName']} />
           </div>
         </div>
-      </div>
-      <div className="row">
-        <div className="col-4">
-          <TextField name="city" path={['fields', 'city']} />
+        <div className="row">
+          <div>
+            <div className="col-10">
+              <TextField name="address" path={['fields', 'address']} />
+            </div>
+            <div className="col-2">
+              <TextField name="zipCode" path={['fields', 'zipCode']} />
+            </div>
+          </div>
         </div>
-        <div className="col-4">
-          <DropDownField name="state" path={['fields', 'state']} />
+        <div className="row">
+          <div className="col-4">
+            <TextField name="city" path={['fields', 'city']} />
+          </div>
+          <div className="col-4">
+            <DropDownField name="state" path={['fields', 'state']} />
+          </div>
+          <div className="col-4">
+            <DropDownField name="country" path={['fields', 'country']} />
+          </div>
         </div>
-        <div className="col-4">
-          <DropDownField name="country" path={['fields', 'country']} />
-        </div>
-      </div>
-      <div className="row">
-        <div className="col-6">
-          <PhoneNumberField
-            phoneNumberProps={{
-              name: 'phoneNumber',
-              path: ['fields', 'phoneNumber']
-            }}
-            phoneNumberCodeProps={{
-              name: 'phoneNumberCode',
-              path: ['fields', 'phoneNumberCode']
-            }}
-          />
+        <div className="row">
+          <div className="col-6">
+            <PhoneNumberField
+              phoneNumberProps={{
+                name: 'phoneNumber',
+                path: ['fields', 'phoneNumber']
+              }}
+              phoneNumberCodeProps={{
+                name: 'phoneNumberCode',
+                path: ['fields', 'phoneNumberCode']
+              }}
+            />
+          </div>
         </div>
       </div>
     </div>
